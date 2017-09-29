7f9ca4b2bd291f5277f4766729bc56f1c168b6cb
Valentin Amé <valentin.ame@kubity.com>
2017-09-29 08:54:34 +0200

    add less support for dev, prod incoming

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -21,7 +21,7 @@ module.exports = () => {
         module: {
             rules: [
                 {
-                    test: /\.css$/,
+                    test: /\.less$/,
                     use: [
                         'style-loader',
                         'css-loader',
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,15 +1,19 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const MinifyPlugin = require('babel-minify-webpack-plugin');
 const webpack = require('webpack');
 
 const baseConfig = require('./webpack.base.config')();
 const modernConfig = require('./webpack.base.config')();
 
+const extractLess = new ExtractTextPlugin('bundle.css');
+
 baseConfig.plugins.push(new MinifyPlugin());
 baseConfig.plugins.push(new webpack.DefinePlugin({
     'process_env': {
         'NODE_ENV': JSON.stringify('production'),
     },
-}))
+}));
+baseConfig.plugins.push(extractLess);
 baseConfig.module.rules[1].use[0].options.presets.push([
     'env', {
         targets: {
@@ -21,6 +25,22 @@ baseConfig.module.rules[1].use[0].options.presets.push([
         },
     },
 ]);
+baseConfig.module.rules[0] = {
+    test: /\.less$/,
+    use: extractLess.extract({
+        use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader' },
+            {
+                loader: 'postcss-loader',
+                options: {
+                    plugins: loader => [require('autoprefixer')()]
+                },
+            },
+            { loader: 'less-loader' },
+        ],
+    }),
+};
 
 modernConfig.output.filename = 'bundle_es6.js';
 modernConfig.plugins.push(new MinifyPlugin());
